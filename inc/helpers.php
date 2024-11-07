310cec58bd353098b1426a4e0606882721555211
Vinny Green <vinnysgreen@gmail.com>
2024-11-06 19:57:14 -0800

    Update helpers.php

--- inc/helpers.php
+++ inc/helpers.php
@@ -1,51 +1,24 @@
 <?php
 /**
- * Helper functions for Ads.txt.
+ * Helper functions for Maker.json.
  *
- * @package Ads_Txt_Manager
+ * @package Maker_Json_Manager
  */
 
-namespace AdsTxt;
+namespace MakerJson;
 
 /**
- * Display the contents of /ads.txt when requested.
+ * Display the contents of /maker.json when requested.
  *
  * @return void
  */
-function display_ads_txt() {
+function display_maker_json() {
 	$request = isset( $_SERVER['REQUEST_URI'] ) ? esc_url_raw( wp_unslash( $_SERVER['REQUEST_URI'] ) ) : false;
-	if ( '/ads.txt' === $request || '/ads.txt?' === substr( $request, 0, 9 ) ) {
-		$post_id = get_option( ADS_TXT_MANAGER_POST_OPTION );
+	if ( '/maker.json' === $request || '/maker.json?' === substr( $request, 0, 11 ) ) {
+		$post_id = get_option( MAKER_JSON_MANAGER_POST_OPTION );
 
-		// Set custom header for ads-txt
-		header( 'X-Ads-Txt-Generator: https://wordpress.org/plugins/ads-txt/' );
-
-		// Will fall through if no option found, likely to a 404.
-		if ( ! empty( $post_id ) ) {
-			$post = get_post( $post_id );
-
-			if ( ! $post instanceof \WP_Post ) {
-				return;
-			}
-
-			header( 'Content-Type: text/plain' );
-			$adstxt = $post->post_content;
-
-			/**
-			 * Filter the ads.txt content.
-			 *
-			 * @since 1.2.0
-			 *
-			 * @param type  $adstxt The existing ads.txt content.
-			 */
-			echo esc_html( apply_filters( 'ads_txt_content', $adstxt ) );
-			die();
-		}
-	} elseif ( '/app-ads.txt' === $request || '/app-ads.txt?' === substr( $request, 0, 13 ) ) {
-		$post_id = get_option( APP_ADS_TXT_MANAGER_POST_OPTION );
-
-		// Set custom header for ads-txt
-		header( 'X-Ads-Txt-Generator: https://wordpress.org/plugins/ads-txt/' );
+		// Set custom header for maker.json
+		header( 'X-Maker-Json-Generator: https://wordpress.org/plugins/maker-json/' );
 
 		// Will fall through if no option found, likely to a 404.
 		if ( ! empty( $post_id ) ) {
@@ -55,22 +28,19 @@ function display_ads_txt() {
 				return;
 			}
 
-			header( 'Content-Type: text/plain' );
-			$adstxt = $post->post_content;
+			header( 'Content-Type: application/json' );
+			$makerjson_content = $post->post_content;
 
 			/**
-			 * Filter the app-ads.txt content.
-			 *
-			 * @since 1.3.0
-			 *
-			 * @param type  $app_adstxt The existing ads.txt content.
+			 * Placeholder for JSON parsing logic.
+			 * Consider validating and encoding JSON data here.
 			 */
-			echo esc_html( apply_filters( 'app_ads_txt_content', $adstxt ) );
+			echo apply_filters( 'maker_json_content', $makerjson_content );
 			die();
 		}
 	}
 }
-add_action( 'init', __NAMESPACE__ . '\display_ads_txt' );
+add_action( 'init', __NAMESPACE__ . '\display_maker_json' );
 
 /**
  * Add custom capabilities.
@@ -85,8 +55,8 @@ function add_capabilities() {
 		return;
 	}
 
-	if ( ! $role->has_cap( ADS_TXT_MANAGE_CAPABILITY ) ) {
-		$role->add_cap( ADS_TXT_MANAGE_CAPABILITY );
+	if ( ! $role->has_cap( MAKER_JSON_MANAGE_CAPABILITY ) ) {
+		$role->add_cap( MAKER_JSON_MANAGE_CAPABILITY );
 	}
 }
 add_action( 'admin_init', __NAMESPACE__ . '\add_capabilities' );
@@ -105,19 +75,19 @@ function remove_capabilities() {
 		return;
 	}
 
-	$role->remove_cap( ADS_TXT_MANAGE_CAPABILITY );
+	$role->remove_cap( MAKER_JSON_MANAGE_CAPABILITY );
 }
 register_deactivation_hook( __FILE__, __NAMESPACE__ . '\remove_capabilities' );
 
 /**
- * Add a query var to detect when ads.txt has been saved.
+ * Add a query var to detect when maker.json has been saved.
  *
  * @param array $qvars Array of query vars.
  *
  * @return array Array of query vars.
  */
 function add_query_vars( $qvars ) {
-	$qvars[] = 'ads_txt_saved';
+	$qvars[] = 'maker_json_saved';
 	return $qvars;
 }
 add_filter( 'query_vars', __NAMESPACE__ . '\add_query_vars' );
